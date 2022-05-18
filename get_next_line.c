a12ecd0363d1606e5f3ec49616b541e7e31dad14
Adrian MANASSERO <amanasse@e3r12p13.clusters.42paris.fr>
2022-05-18 13:15:46 +0200

    mise a jour get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,41 +6,91 @@
 /*   By: amanasse <amanasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:35:47 by amanasse          #+#    #+#             */
-/*   Updated: 2022/05/17 19:10:54 by amanasse         ###   ########.fr       */
+/*   Updated: 2022/05/18 13:14:13 by amanasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#define BUFFER_SIZE 10
+
+char *stock_save(char *dest, int octet)
+{
+
+	int	i;
+	int j;
+	char	*save;
+	char	*line;
+	
+	i = 0;
+	save = malloc(sizeof(char) * (octet + 2));
+	line = malloc(sizeof(char) * (octet + 2));
+	while (dest[i] != '\0' && octet > 0)
+	{
+		save[i] = dest[i];
+		line[i] = save[i];
+		if (save[i] == '\n')
+			break ;
+		i++;
+		octet--;
+	}
+	line[i] = '\0';
+	free(save);
+	save = malloc(sizeof(char) * (octet + 2));
+	j = 0;
+	while (dest[i + j] != '\0' || octet > 0)
+	{
+		save[j] = dest[i + j];
+		j++;
+		octet--;
+	}
+	save[j] = '\0';
+	ft_putstr(line);
+	puts ("\n");
+	ft_putstr(save);
+	puts ("\n");
+	return(line);
+	
+
+
+}
+
+char	*get_next_line(int fd)
+{
+	char		*dest;
+	char		*line;
+	int			nb_octet;
+	static char	*save;
+	
+
+
+	nb_octet = 1;
+	if(fd == -1 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
+		return(0);	
+	dest = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	nb_octet = read(fd, dest, BUFFER_SIZE);
+	line = stock_save(dest, nb_octet);
+
+	free(dest);
+	return(line);
+}
 
 int	main() 
 {
 	int	fichier;
-	int nb_octet;
-	int count;
-	char dest[BUFFER_SIZE + 1];
 
-	nb_octet = 1;
-	count = nb_octet;
 	if ((fichier = open("test.txt", O_RDONLY)) == -1) 
 	{
 		perror("Impossible d'ouvrir le fichier en lecture\n"); 
 		exit(1);
 	}
-
+	ft_putnbr(fichier);
 	puts("Traitement...");
-	while (nb_octet != 0)
-	{
-		nb_octet = read(fichier, dest, BUFFER_SIZE);
-		// ft_putnbr(nb_octet);
-		puts ("\n");
-		dest[nb_octet] = '\0';
-		ft_putstr(dest);
-		puts ("\n");
-		count = nb_octet + count;
-	}
-	ft_putnbr(nb_octet);
-	ft_putnbr(count);
+
+	get_next_line(fichier);
+
+	
+
 	close(fichier);
 	puts("FIN"); 
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: amanasse <amanasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:35:00 by amanasse          #+#    #+#             */
-/*   Updated: 2022/05/17 19:10:52 by amanasse         ###   ########.fr       */
+/*   Updated: 2022/05/18 10:32:12 by amanasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,43 +56,44 @@ void	ft_putstr(char *str)
 	}
 }
 
-// char	*ft_strjoin(char *s1, char *s2)
 
-// {
-// 	size_t	i;
-// 	size_t	j;
-// 	size_t	lens1;
-// 	size_t	lens2;
-// 	char	*str;
+size_t	ft_strlen(const char *s)
+{
+	int	i;
 
-// 	lens1 = ft_strlen(s1);
-// 	lens2 = ft_strlen(s2);
-// 	str = malloc(sizeof(char) * ((lens1 + lens2) + 1));
-// 	if (!str)
-// 		return (NULL);
-// 	i = 0;
-// 	j = 0;
-// 	while (s1[i])
-// 	{
-// 		str[i] = s1[i];
-// 		i++;
-// 	}
-// 	while (s2[j])
-// 	{
-// 		str[i + j] = s2[j];
-// 		j++;
-// 	}
-// 	str[i + j] = '\0';
-// 	return (str);
-// }
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
 
+char	*ft_strjoin(char *s1, char *s2)
 
-// size_t	ft_strlen(const char *s)
-// {
-// 	int	i;
+{
+	size_t	i;
+	size_t	j;
+	size_t	lens1;
+	size_t	lens2;
+	char	*str;
 
-// 	i = 0;
-// 	while (s[i] != 0)
-// 		i++;
-// 	return (i);
-// }
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	str = malloc(sizeof(char) * ((lens1 + lens2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
